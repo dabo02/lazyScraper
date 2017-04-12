d5a4cdb0f89d34f368a44aee82966aa71ca59665
dabo02 <dabo_02@live.com>
2017-04-12 19:56:00 -0400

    commented code

--- allitebooks-scraper.py
+++ allitebooks-scraper.py
@@ -4,6 +4,19 @@ from bs4 import BeautifulSoup
 
 
 class MyLazyBookScraper:
+    """
+     Scrapes allitebooks.com and downloads the books for the given query.
+     Attributes:
+         url (str): contains alliteebooks link.
+         download_path (str): contains the path/for/pdf/downloads.
+         query (str): contains the query for allitebooks.
+         html (file): contains the html file for which to scrape.
+         first_traverse (bs4-Object): beautifulsoup object with scraped parsed html file.
+         all_links_page_1 (bs4-Object): object containing all anchor tags for parsed html file.
+         paginator_links (str): contains all scraped href strings of all pages on the site.
+         links_needed_for_page (str): contains all scraped href strings for all the books in given link from paginator_links.
+         traverser (array): contains all bs4 objects for book download.
+    """
 
     def __init__(self, query, download_path):
         self.url = 'http://www.allitebooks.com/'
@@ -19,6 +32,7 @@ class MyLazyBookScraper:
         self.links_needed_for_page = []
 
     def first_traverse(self):
+        """ traverses the page for the first time and collects links needed for book download."""
         for link in self.all_links_page_1:
             if link.get('rel') == ['bookmark']:
                 self.links_needed_for_page.append(link.get('href'))
@@ -26,17 +40,20 @@ class MyLazyBookScraper:
                 self.paginator_links.append(link.get('href'))
         return self.paginator_links
     
-    def get_links(self, link):
-        html = urlopen(link)
+    def get_links(self, link_of_page):
+        """ returns all links to be scraped for books."""
+        html = urlopen(link_of_page)
         traverse = BeautifulSoup(html, 'html.parser')
         return traverse.find_all('a')
     
     def set_links(self, links):
+        """ sets links needed for pdf download."""
         for link in links:
             if link.get('rel') == ['bookmark']:
                 self.links_needed_for_page.append(link.get('href'))
     
     def download_books(self, traverser):
+        """ takes list of traversed books for download."""
         for bsObject in traverser:
             all_links_from_page = bsObject.find_all('a')
             title = bsObject.title.string
@@ -50,7 +67,9 @@ class MyLazyBookScraper:
                             print(title + ": Downloaded")
                         except urllib.error.HTTPError:
                             continue
+
     def fix_paginator_links(self, pagination):
+        """ traverses the page for the first time and collects links needed for book download."""
         first_link = self.url + '?s=' + self.query
         last_link = pagination[len(pagination)-1]
         self.paginator_links = []
@@ -65,6 +84,7 @@ class MyLazyBookScraper:
                 self.paginator_links.append(self.url +'page/' + str(i) + '?s=' + self.query)
 
     def all_traverses(self, links):
+        """ traverses the list of paginator links and collects links needed for book download."""
         self.traverser = []
         for link in links:
             tmp = BeautifulSoup(urlopen(link), 'html.parser')
